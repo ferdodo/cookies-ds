b8086a36576250f4873a31e880c261868efa13ac
Thomas Riffard <ferdodo@hotmail.fr>
2023-04-26 22:24:11 +0200

    wait  window to be focused if it's not visible at startup for initial animations

--- utils/app-ready.ts
+++ utils/app-ready.ts
@@ -1,3 +1,41 @@
-export const appReady = new Promise(function(resolve) {
-	setTimeout(resolve, 800);
-});
+import { fromEvent, Observable, filter, map, mapTo, combineLatest, merge, first } from "rxjs";
+
+function isWindowVisible(): boolean {
+	return !document.hidden;
+}
+
+function visibleAndFocusedObservable(): Observable<boolean> {
+	const visibilityChange$ = fromEvent(document, "visibilitychange");
+
+	const focus$ = fromEvent(window, "focus")
+		.pipe(mapTo(true));
+
+	const blur$ = fromEvent(window, "blur")
+		.pipe(mapTo(false));
+
+	const focused$ = merge(focus$, blur$);
+
+	return combineLatest(visibilityChange$, focused$)
+		.pipe(
+			map(([, focused]) => isWindowVisible() && focused),
+			filter(visible => visible),
+			first()
+		);
+}
+
+async function waitVisible() {
+	if (isWindowVisible()) {
+		return;
+	}
+
+	await visibleAndFocusedObservable()
+		.toPromise();
+}
+
+async function waitAppReady() {
+	await new Promise(r => setTimeout(r, 400));
+	await waitVisible();
+	await new Promise(r => setTimeout(r, 400));
+}
+
+export const appReady = waitAppReady();
