b79433bb0a64da88075d85e0fb57c247616cd004
Thomas Riffard <ferdodo@hotmail.fr>
2023-09-30 16:31:35 +0200

    add loading state for panels

--- components/panel/index.ts
+++ components/panel/index.ts
@@ -16,27 +16,89 @@ const templateNode: HTMLTemplateElement = document.createElement("template");
 templateNode.innerHTML = template;
 
 export class Panel extends HTMLElement {
+	contentTimeoutElapsed: boolean = false;
+	contentLoaded: boolean = false;
+	connected: boolean = false;
+	loading: string = "100";
+
+	static get observedAttributes() {
+		return ["loading"];
+	}
+
 	async connectedCallback() {
 		this.attachShadow({ mode: "open" });
 		await appReady;
 		const release = await acquireAnimationLock();
 		const shadowRoot: ShadowRoot = getShadowRoot(this);
 		shadowRoot.appendChild(templateNode.content.cloneNode(true));
+		const loading: string | null = this.getAttribute("loading");
 
-		setTimeout(function() {
+		if (loading !== null && isLoadingValid(loading)) {
+			this.loading = loading;
+		}
+
+		setTimeout(() => {
 			const panelContainer: HTMLElement = getElement(shadowRoot, "#panel-container");
 			panelContainer.style.gridTemplateColumns = "1rem 1fr 1rem";
 			panelContainer.style.gridTemplateRows = "1rem 1fr 1rem";
 			const panel: HTMLElement = getElement(shadowRoot, "#panel");
-			panel.style.backgroundColor = "#ffffff80";
+			panel.style.backgroundColor = "#ffffff40";
+			this.render();
 		}, 10);
 
-		setTimeout(function() {
-			const panelContent: HTMLElement = getElement(shadowRoot, "#panel-content");
-			panelContent.style.visibility = "visible";
-			panelContent.style.opacity = "1";
+		setTimeout(() => {
+			this.contentTimeoutElapsed = true;
+			this.render();
 		}, 400);
 
 		setTimeout(release, 600);
+		this.connected = true;
+		this.render();
 	}
+
+	render() {
+		const shadowRoot: ShadowRoot = getShadowRoot(this);
+		const panelContent: HTMLElement = getElement(shadowRoot, "#panel-content");
+		const panelLoading: HTMLElement = getElement(shadowRoot, "#panel-loading");
+		const panel: HTMLElement = getElement(shadowRoot, "#panel");
+
+		if (this.contentTimeoutElapsed && isLoaded(this.loading)) {
+			panelContent.style.visibility = "visible";
+			panelContent.style.opacity = "1";
+			panel.style.overflow = "auto";
+		} else {
+			panelContent.style.visibility = "collapse";
+			panelContent.style.opacity = "0";
+			panel.style.overflow = "hidden";
+		}
+
+		panelLoading.style.width = `${this.loading}%`;
+	}
+
+	attributeChangedCallback() {
+		if (this.shadowRoot && this.connected) {
+			const loading: string | null = this.getAttribute("loading");
+
+			if (loading !== null && isLoadingValid(loading)) {
+				this.loading = loading;
+			}
+
+			this.render();
+		}
+	}
+}
+
+function isLoadingValid(loading: string | null): boolean {
+	const loadingAsNumber: number = Number(loading);
+	const rounded: number = Math.floor(loadingAsNumber);
+
+	return rounded == loadingAsNumber
+		&& Number.isInteger(rounded)
+		&& Number.isFinite(rounded)
+		&& rounded <= 100
+		&& rounded >= 0;
+}
+
+function isLoaded(loading: string | null): boolean {
+	return loading === null || loading === "100";
 }
