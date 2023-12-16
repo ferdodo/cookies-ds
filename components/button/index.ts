6b7eb7f4528ab820e10741405959737dbddb0041
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-16 13:11:50 +0100

    remove confetti fron button

--- components/button/index.ts
+++ components/button/index.ts
@@ -1,16 +1,12 @@
 import template from "./template.js";
 import { notesSansLoaded } from "../../fonts/index.js";
 import { getShadowRoot } from "../../utils/get-shadow-root.js";
-import { getElement } from "../../utils/get-element.js";
-import throwConfetti from "canvas-confetti";
-import { fromEvent, throttleTime, Subscription, take } from "rxjs";
+import { Subscription } from "rxjs";
 
 declare global {
 	export namespace JSX {
 		export interface IntrinsicElements {
-			"cookies-button": {
-				confetti: boolean;
-			};
+			"cookies-button": {};
 		}
 	}
 }
@@ -20,32 +16,12 @@ templateNode.innerHTML = template;
 
 export class Button extends HTMLElement {
 	clickSubscription: Subscription | null = null;
-	confetti: boolean = false;
-
-	static get observedAttributes() {
-		return ["confetti"];
-	}
 
 	async connectedCallback() {
 		await notesSansLoaded;
 		this.attachShadow({ mode: "open" });
 		const shadowRoot = getShadowRoot(this);
 		shadowRoot.appendChild(templateNode.content.cloneNode(true));
-		const button = getElement(shadowRoot, "button");
-		this.confetti = this.getAttribute("confetti") !== null;
-
-		const clickObservable = fromEvent(button, "click")
-			.pipe(throttleTime(100), take(7));
-
-		this.clickSubscription = clickObservable.subscribe(() => {
-			if (this.confetti) {
-				throwConfetti();
-			}
-		});
-	}
-
-	attributeChangedCallback() {
-		this.confetti = this.getAttribute("confetti") !== null;
 	}
 
 	disconnectedCallback() {
